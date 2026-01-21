f3201f5e9a3f25e1293ca0171ebe5dc8e36007d4
Nova Lightsilver <83142624+Alienxcript@users.noreply.github.com>
2026-01-21 21:25:21 +0100

    modified the python script for file handling

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,7 @@
 # password generator
 
 import random
+from datetime import datetime
 
 char = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*_-+=|,./?><;:"
 
@@ -14,6 +15,7 @@ user_no_of_char = 0
 def intro():
     print("Hello, welcome to the Password Generator")
 
+# this function checks the user input if it a number(integer)
 
 def no_of_char():
     global user_no_of_char
@@ -25,6 +27,8 @@ def no_of_char():
     else:
         confirm_no_char()
 
+# this function checks if the imputed integer is less than seven
+
 def confirm_no_char():
     if user_no_of_char < 7:
         print("Your value is less than seven(7)")
@@ -35,6 +39,8 @@ def confirm_no_char():
 
 pas_wrd = list()
 
+# this function generates the password using the user stated passowrd number length with a while loop 
+# and appends it to the above password variable that is a list
 
 def password_generate():
     global pas_wrd
@@ -44,13 +50,26 @@ def password_generate():
         pas_wrd.append(choosed_char)
         user_no_of_char -= 1 
 
+# the functions executed 
+
+#function to save the passwords
+
+def save_password(password):
+    with open("generated_passwords.txt", "a") as file:
+        file.write("Password:  " + password + "\n")
+        file.write("Time Generated:  " + str(datetime.now()) + "\n\n")
+
+
 intro()
 no_of_char()
 pas_wrd = "".join(pas_wrd)
 print(pas_wrd)
+save_password(pas_wrd)
 
 
+
+#seems this was necessary at the time when the script is launched through the terminal
+# after the output the terminal closes this code is to keep it open
 input("Press Enter to Exit:.....    ")
-
 
 
